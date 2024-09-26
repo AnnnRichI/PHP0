2e5a23c3ace960aaed1eb825714dc4e86a2d5a45
NastyaBabakova <nastya.babakova.2001@mail.ru>
2024-09-26 13:57:54 +0700

    done

--- 2.php
+++ 2.php
@@ -22,14 +22,14 @@
  */
 
 
- for ($i = 1; $i <= 100; $i++) {
+for ($i = 1; $i <= 100; $i++) {
     if ($i % 15 == 0) {
-        echo 'FizzBuzz'. PHP_EOL . $i . PHP_EOL;
+        echo 'FizzBuzz' . PHP_EOL;
     } else if ($i % 3 == 0) {
-        echo 'Fizz' . PHP_EOL . $i . PHP_EOL;
+        echo 'Fizz' . PHP_EOL;
     } else if ($i % 5 == 0) {
-        echo "Buzz". PHP_EOL . $i . PHP_EOL;
+        echo "Buzz" . PHP_EOL;
     } else {
-        echo $i . PHP_EOL ;
+        echo $i . PHP_EOL;
     }
- }
+}
