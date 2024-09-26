61b98cd08c1d8ad7355cc9b3a23d86e407d2e8e2
NastyaBabakova <nastya.babakova.2001@mail.ru>
2024-09-26 13:43:02 +0700

    done hw

---
+++ 4.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Дана строка $str = 'abcdef'; 
+* напишите алгоритм, преобразующий строку 
+* в перевернутом виде чтобы echo $str; вывел fedcba
+*/
+
+$str = "hello";
+$result = '';
+
+for ($i = strlen($str) - 1; $i >= 0; $i--) {
+    echo $str[$i];
+}
+
+echo $result;
